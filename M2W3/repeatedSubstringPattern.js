b60bdc82d85a64c5b4ea056f0cb49d77fbcc7d6c
Anshu Jain <48069738+anshuj11@users.noreply.github.com>
2020-09-09 19:43:12 -0700

    Revised first time - 9/9/20

--- M2W3/repeatedSubstringPattern.js
+++ M2W3/repeatedSubstringPattern.js
@@ -1,19 +1,18 @@
 var repeatedSubstringPattern = function(s) {
-   let dp = new Array(s.length).fill(0)
-   dp[0] = 0;
-   let lL;
+    let dp = new Array(s.length).fill(0);
+    let lL;
+    for(let i = 1; i < s.length; i++) {
+        lL = dp[i - 1]
+        while(lL > 0 && s[i] !== s[lL]){
+            lL = dp[lL - 1]
+        }
+        if(s[i] === s[lL]){
+            lL++
+        }
+        dp[i] = lL
+    }
     
-   for( let i = 1; i < s.length; i++) {
-       lL = dp[i - 1]
-       while( lL > 0 && s[i] !== s[lL]){
-           lL = dp[lL - 1]
-       }
-       if(s[i] === s[lL])
-           lL++
-       dp[i] = lL
-   }
+    lL = dp[s.length - 1]
     
-   lL = dp[s.length - 1] 
-    
-   return (lL > 0 ) && (s.length %(s.length - lL) === 0)
+    return (lL > 0) && ( s.length%(s.length - lL) === 0)   
 };
