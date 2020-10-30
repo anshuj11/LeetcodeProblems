83f135699d321a8a1a74b887f6e2a1c97c359bce
Anshu Jain <48069738+anshuj11@users.noreply.github.com>
2020-10-30 14:04:17 -0700

    177. Nth Highest Salary
    Runtime: 483 ms, faster than 12.88% of MySQL online submissions for Nth Highest Salary.
    Memory Usage: 0B, less than 100.00% of MySQL online submissions for Nth Highest Salary.

---
+++ M4W2/getNthHighestSalary.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE M INT;
+  SET M=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      
+      select distinct salary
+      from employee
+      order by salary desc
+      limit M, 1
+      
+  );
+END
